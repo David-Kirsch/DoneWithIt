aee4227cfd8fd9ef1fc36e5fb8b54fda1d80e0c6
David-Kirsch <davidjkirsch@gmail.com>
2021-01-10 20:04:56 -0500

    Created Welcome Screen

--- App.js
+++ App.js
@@ -1,42 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  TouchableWithoutFeedback,
-  Image,
-  TouchableOpacity,
-  TouchableHighlight,
-} from "react-native";
+import { View, Image } from "react-native";
+import WelcomeScreen from "./app/Screens/WelcomeScreen";
 
 export default function App() {
-  console.log(require("./assets/icon.png"));
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello World!</Text>
-      <TouchableHighlight onPress={() => console.log("tapped")}>
-        <Image
-          blurRadius={1}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
